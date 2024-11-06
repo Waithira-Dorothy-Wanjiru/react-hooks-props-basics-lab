4d01eb2425f7a7c8091829b2c1acd198a09c23ce
Dorothy <dorothywanjiru32@gmail.com>
2024-11-06 21:34:45 +0300

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import user from "../data/user";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{user.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <a href={user.links} target="_blank" >GitHub</a>
+      <a href={user.links} target="_blank" >LinkedIn</a>
+  
     </div>
   );
 }
